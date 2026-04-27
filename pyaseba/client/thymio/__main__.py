13b2f28d5a2c87eb0f2820299482894d1aa7691e
Jérôme Guzzi <jerome@idsia.ch>
2026-04-27 17:35:43 +0200

    Added target

--- pyaseba/client/thymio/__main__.py
+++ pyaseba/client/thymio/__main__.py
@@ -1,9 +1,14 @@
+import argparse
+
 from ..node.shell import NodeShell
 from .thymio import Thymio
 
 if __name__ == '__main__':
     node = Thymio()
-    node.connect(target='ser:name=Thymio')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--target', default="ser:name=Thymio")
+    args = parser.parse_args()
+    node.connect(target=args.target)
     shell = NodeShell(node)
     shell.prompt = '(thymio)'
     shell.cmdloop()
